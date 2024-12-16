9b0664c200ef92bf27bc7b8f5cd4bf17391b54e3
Fabio Serragnoli <fabio.serragnoli@sky.uk>
2024-12-16 18:24:41 +0000

    Exercise 3.18 - Done

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -121,7 +121,7 @@ object List: // `List` companion object. Contains functions for creating and wor
 
   def doubleToString(l: List[Double]): List[String] = foldRight(l, Nil: List[String],(d, acc) => Cons(d.toString, acc))
 
-  def map[A, B](l: List[A], f: A => B): List[B] = ???
+  def map[A, B](l: List[A], f: A => B): List[B] = foldRight(l, Nil: List[B], (a, acc) => Cons(f(a), acc))
 
   def filter[A](as: List[A], f: A => Boolean): List[A] = ???
 
