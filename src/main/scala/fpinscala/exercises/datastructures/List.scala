3da45042e5954ce9df4eb80ef9bf364981aeda7f
Fabio Serragnoli <fabio.serragnoli@sky.uk>
2024-12-16 18:55:49 +0000

    Exercise 3.22 - Done

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -132,9 +132,14 @@ object List: // `List` companion object. Contains functions for creating and wor
   def flatMap[A, B](as: List[A], f: A => List[B]): List[B] =
     foldRight(as, Nil: List[B], (i, acc) => append(f(i), acc))
 
-  def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] = flatMap(as, a => if f(a) then List(a) else Nil)
+  def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] =
+    flatMap(as, a => if f(a) then List(a) else Nil)
 
-  def addPairwise(a: List[Int], b: List[Int]): List[Int] = ???
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (_, Nil)            => Nil
+    case (Nil, _)            => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+  }
 
   // def zipWith - TODO determine signature
 
