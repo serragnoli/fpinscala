cbc7688f19717e79d6643db8c06b75336958000a
Fabio Serragnoli <fabio@serragnoli.com>
2024-12-18 13:18:07 +0000

    Exercise 3.25 - Done

--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -5,29 +5,37 @@ enum Tree[+A]:
   case Branch(left: Tree[A], right: Tree[A])
 
   def size: Int = this match
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
     case Branch(l, r) => 1 + l.size + r.size
 
   def depth: Int = ???
 
   def map[B](f: A => B): Tree[B] = ???
 
-  def fold[B](f: A => B, g: (B,B) => B): B = ???
-  
+  def fold[B](f: A => B, g: (B, B) => B): B = ???
+
   def sizeViaFold: Int = ???
-  
+
   def depthViaFold: Int = ???
-  
+
   def mapViaFold[B](f: A => B): Tree[B] = ???
 
 object Tree:
 
   def size[A](t: Tree[A]): Int = t match
-    case Leaf(_) => 1
-    case Branch(l,r) => 1 + size(l) + size(r)
+    case Leaf(_)      => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
 
   extension (t: Tree[Int]) def firstPositive: Int = ???
 
-  extension (t: Tree[Int]) def maximum: Int = ???
+  extension (t: Tree[Int])
+    def maximum: Int = t match {
+      case Leaf(v) => v
+      case Branch(left, right) => {
+        val lMax = left.maximum
+        val rMax = right.maximum
+        if (lMax > rMax) lMax else rMax
+      }
+    }
 
   extension (t: Tree[Int]) def maximumViaFold: Int = ???
